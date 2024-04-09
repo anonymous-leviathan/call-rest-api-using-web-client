95fed6887cbe7d56938e37499a424fb6e2a3fa92
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-04-09 18:27:04 +0530

    initial commit

---
+++ src/main/java/org/example/call_rest_api_using_web_client/CallRestApiUsingWebClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.call_rest_api_using_web_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallRestApiUsingWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallRestApiUsingWebClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/call_rest_api_using_web_client/CallRestApiUsingWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.call_rest_api_using_web_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallRestApiUsingWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
